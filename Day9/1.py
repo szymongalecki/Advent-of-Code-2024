b2085584b8f5e18b25917af5c40cc4788c153947
Szymon Gałecki <szymongalecki@gmail.com>
2025-09-24 22:38:52 +0200

    Day 9 part 1

--- Day9/1.py
+++ Day9/1.py
@@ -5,38 +5,31 @@ def data() -> list[int]:
     return [int(d) for d in open(sys.argv[1], "r").readline()]
 
 
-def layout(data: list[int]) -> str:
-    ly = ""
+def layout(data: list[int]) -> list[int | None]:
+    ly = []
     for i, d in enumerate(data):
         if i % 2 == 0:
-            # fix: breaks layout for digits bigger than 9
-            ly += str(i // 2) * d
+            ly += [i // 2] * d
         else:
-            ly += "." * d
+            ly += [None] * d
     return ly
 
 
-def compact(layout: str) -> str:
-    ly = list(layout)
+def compact(layout: list[int | None]) -> list[int | None]:
     l = 0
-    r = len(ly) - 1
+    r = len(layout) - 1
     while l < r:
-        if ly[l] == "." and ly[r] != ".":
-            ly[l], ly[r] = ly[r], ly[l]
-        if ly[l] != ".":
+        if layout[l] is None and layout[r] is not None:
+            layout[l], layout[r] = layout[r], layout[l]
+        if layout[l] is not None:
             l += 1
-        if ly[r] == ".":
+        if layout[r] is None:
             r -= 1
-    return "".join(ly)
+    return layout
 
 
-def checksum(compact: str) -> int:
-    return sum(i * int(d) for i, d in enumerate(list(compact)) if d != ".")
+def checksum(compact: list[int | None]) -> int:
+    return sum(i * int(d) for i, d in enumerate(compact) if d)
 
-
-example = [int(d) for d in "2333133121414131402"]
-assert layout(example) == "00...111...2...333.44.5555.6666.777.888899"
-assert compact(layout(example)) == "0099811188827773336446555566.............."
-assert checksum(compact(layout(example))) == 1928
 
 print(f"Part one: {checksum(compact(layout(data())))}")
